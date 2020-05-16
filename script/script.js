15fa21a039c0cc22b9658903f7b028ef9d0c62e9
Viacheslav Denysko <deniskosv@gmail.com>
2020-05-16 15:02:35 +0300

    done day2

--- script/script.js
+++ script/script.js
@@ -5,7 +5,10 @@ window.addEventListener('DOMContentLoaded', () => {
 	const modalBlock = document.getElementById('modalBlock'),
 		questionTitle = document.getElementById('question'),
 		formAnswers = document.getElementById('formAnswers'),
-		menuButton = document.getElementById('burger');
+		menuButton = document.getElementById('burger'),
+		prevBtn = document.getElementById('prev'),
+		nextBtn = document.getElementById('next'),
+		sendBtn = document.getElementById('send');
 
 	const handleWidth = () => {
 		const clientWidth = document.documentElement.clientWidth;
@@ -15,29 +18,137 @@ window.addEventListener('DOMContentLoaded', () => {
 	handleWidth();
 
 	const playTest = () => {
-		const renderQuestions = () => {
-			questionTitle.textContent = 'Какого цвета бургер вы хотите?';
+
+		let numberQuestion = 0;
+
+		const questions = [
+			{
+				question: "Какого цвета бургер?",
+				answers: [
+					{
+						title: 'Стандарт',
+						url: './image/burger.png'
+					},
+					{
+						title: 'Черный',
+						url: './image/burgerBlack.png'
+					}
+				],
+				type: 'radio'
+			},
+			{
+				question: "Из какого мяса котлета?",
+				answers: [
+					{
+						title: 'Курица',
+						url: './image/chickenMeat.png'
+					},
+					{
+						title: 'Говядина',
+						url: './image/beefMeat.png'
+					},
+					{
+						title: 'Свинина',
+						url: './image/porkMeat.png'
+					}
+				],
+				type: 'radio'
+			},
+			{
+				question: "Дополнительные ингредиенты?",
+				answers: [
+					{
+						title: 'Помидор',
+						url: './image/tomato.png'
+					},
+					{
+						title: 'Огурец',
+						url: './image/cucumber.png'
+					},
+					{
+						title: 'Салат',
+						url: './image/salad.png'
+					},
+					{
+						title: 'Лук',
+						url: './image/onion.png'
+					}
+				],
+				type: 'checkbox'
+			},
+			{
+				question: "Добавить соус?",
+				answers: [
+					{
+						title: 'Чесночный',
+						url: './image/sauce1.png'
+					},
+					{
+						title: 'Томатный',
+						url: './image/sauce2.png'
+					},
+					{
+						title: 'Горчичный',
+						url: './image/sauce3.png'
+					}
+				],
+				type: 'radio'
+			}
+		];
+
+		const renderAnswers = i => {
 			formAnswers.innerHTML = '';
-
-			const data = [
-				{ id: 'answerItem1', name: 'Стандарт', src: './image/burger.png' },
-				{ id: 'answerItem2', name: 'Черный', src: './image/burgerBlack.png' }
-			];
-
-			data.forEach(data => {
+			questions[i].answers.forEach(answers => {
 				const blocks = `
 					<div class="answers-item d-flex flex-column">
-						<input type="radio" id="${data.id}" name="answer" class="d-none">
-						<label for="answerItem1" class="d-flex flex-column justify-content-between">
-							<img class="answerImg" src="${data.src}" alt="burger">
-							<span>${data.name}</span>
+						<input type="${questions[i].type}" id="${answers.title}" name="answer" class="d-none">
+						<label for="${answers.title}" class="d-flex flex-column justify-content-between">
+							<img class="answerImg" src="${answers.url}" alt="burger">
+							<span>${answers.title}</span>
 						</label>
 					</div>
 					`;
 				formAnswers.insertAdjacentHTML('beforeend', blocks);
 			});
 		};
-		renderQuestions();
+
+		const renderQuestions = iQ => {
+			questionTitle.textContent = `${questions[iQ].question}`;
+			renderAnswers(iQ);
+		};
+		renderQuestions(numberQuestion);
+
+		const modalFooter = document.getElementById('modalFooter');
+
+		const checkBtns = () => {
+			if (numberQuestion !== 0) {
+				prevBtn.classList.remove('d-none');
+			} else {
+				prevBtn.classList.add('d-none');
+			}
+
+			if (numberQuestion === questions.length - 1) {
+				nextBtn.classList.add('d-none');
+				sendBtn.classList.remove('d-none');
+			} else {
+				nextBtn.classList.remove('d-none');
+				sendBtn.classList.add('d-none');
+			}
+		};
+		checkBtns();
+
+		modalFooter.addEventListener('click', event => {
+			if (event.target.id === 'prev') {
+				numberQuestion--;
+				checkBtns();
+				renderQuestions(numberQuestion);
+			} else if (event.target.id === 'next') {
+				numberQuestion++;
+				checkBtns();
+				renderQuestions(numberQuestion);
+			}
+		});
+
 	};
 
 	const showDialog = () => {
